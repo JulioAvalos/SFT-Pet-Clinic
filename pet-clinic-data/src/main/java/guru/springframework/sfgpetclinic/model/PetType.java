a205d8eed680e730553a13530fc4e5b27eba0cb4
Julio Avalos <julioavalos2@msn.com>
2019-07-04 22:43:33 -0600

    Adding project lombok to project. closes #51.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,10 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
