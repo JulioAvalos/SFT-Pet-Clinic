2c2b9290350bb1f14e15fb767ac8186f90e8dba5
Julio Avalos <julioavalos2@msn.com>
2019-07-11 21:48:16 -0600

    added create update owner. closes #55

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -10,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -21,5 +22,9 @@ public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public boolean isNew(){
+        return Objects.isNull(this.id);
+    }
 
 }
